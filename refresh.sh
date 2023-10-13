0b76420d14d9d8f3b7204ef345a3b46cbd70d6a3
Mirko Van Colen <mirko@vancolen.com>
2023-10-13 17:36:29 +0200

    add category

--- refresh.sh
+++ refresh.sh
@@ -11,23 +11,36 @@ if [ $? -eq 0 ]; then
     echo "Repository cloned successfully."
 
     # Define source and target directories
-    src_dir="$tmp_dir/vv_compose/data/playbooks"
+    src_base_dir="$tmp_dir/vv_compose/data"
     target_dir="/srv/apps/vv_compose/data"
 
-    # Check if the source directory exists
-    if [ -d "$src_dir" ]; then
-        # Remove the target directory if it already exists
-        if [ -d "$target_dir/playbooks" ]; then
-            echo "Removing existing target directory: $target_dir/playbooks"
-            rm -rf "$target_dir/playbooks"
-        fi
+    # Define an array of directories and files to process
+    items=("playbooks" "forms" "forms.yaml")
 
-        # Move the source directory to the target location
-        mv "$src_dir" "$target_dir"
-        echo "Moved $src_dir to $target_dir"
-    else
-        echo "Source directory $src_dir not found."
-    fi
+    for item in "${items[@]}"; do
+        src_item="$src_base_dir/$item"
+        
+        # Check if the source item exists
+        if [ -e "$src_item" ]; then
+            if [ -d "$src_item" ]; then
+                # Remove the target directory if it already exists
+                if [ -d "$target_dir/$item" ]; then
+                    echo "Removing existing target directory: $target_dir/$item"
+                    rm -rf "$target_dir/$item"
+                fi
+
+                # Move the source directory to the target location
+                mv "$src_item" "$target_dir"
+                echo "Moved $src_item to $target_dir"
+            else
+                # Copy the source file to the target location
+                cp "$src_item" "$target_dir"
+                echo "Copied $src_item to $target_dir"
+            fi
+        else
+            echo "Source item $src_item not found."
+        fi
+    done
 else
     echo "Failed to clone the repository."
 fi
